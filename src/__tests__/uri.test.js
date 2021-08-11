5e23596fa8df42c870e030e632f31b8b3fe6950b
Leon <leonwheeler08@gmail.com>
2021-08-11 17:11:46 +0100

    Created first test for slugified URIs, passed without issues.

--- src/__tests__/uri.test.js
+++ src/__tests__/uri.test.js
@@ -6,3 +6,11 @@ const generateUri = (town, venue) => {
   const uri = `/${slugifiedTown}-events/${slugifiedVenue}`;
   return uri;
 };
+
+describe("Slugified URI", () => {
+  it("should return a slugified version of the URI", () => {
+    const expected = "/bristol-events/thekla";
+    const actual = generateUri("bristol", "thekla");
+    expect(actual).toEqual(expected);
+  });
+});
