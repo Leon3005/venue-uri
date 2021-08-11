3509d180a2e1e435babb47cce2c2e9d912fedd59
Leon <leonwheeler08@gmail.com>
2021-08-11 18:37:46 +0100

    Added 'strict' key to slugify

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ const slugify = require("slugify");
 const generateUri = (town, venue) => {
   const slugifiedTown = slugify(town, {
     lower: true,
+    strict: true,
   });
   const slugifiedVenue = slugify(venue, {
     lower: true,
+    strict: true,
   });
   const uri = `/${slugifiedTown}-events/${slugifiedVenue}`;
   return uri;
